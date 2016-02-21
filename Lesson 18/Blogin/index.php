a4bd9996377f14fdd8694490890e3cebcd9aa6fc
Aleksey Dmitrenko <DIESELOKJ@gmail.com>
2016-02-21 11:11:29 +0200

    WordPress Theme

---
+++ Lesson 18/Blogin/index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blogin.</title>
+    <!--[if lt IE 9]>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/r29/html5.min.js"></script>
+    <![endif]-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link href="styles/style.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="js/bxslider/jquery.bxslider.css">
+
+</head>
+<body>
+
+<header id="navbar" class="navbar">
+    <div id="content-wrap" class="container">
+        <h1><a href="#navbar">Blogin.</a></h1>
+        <nav>
+            <ul class="nav">
+                <div class="iconMenu"><i class="fa fa-bars fa-2x"></i></div>
+                <div class="itemsMenu">
+                    <li><a href="#hero">Journal</a></li>
+                    <li><a href="#intro">About</a></li>
+                    <li><a href="#portfolio">Work</a></li>
+                    <li><a href="#clients">Contact</a></li>
+                </div>
+            </ul>
+        </nav>
+    </div>
+</header>
+
+<div class="container wrap">
+    <aside class="right-aside">
+            <section class="main-descript">
+                <h2>About Blogin.</h2>
+                <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                <p>Excepteur sint occaecat sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </section>
+
+            <section class="search">
+                <h2>Search.</h2>
+                <form method="post" action="#">
+                    <input type="search" name="search" placeholder="What are you looking for?">
+                </form>
+            </section>
+
+            <section class="tuned-mail">
+                <h2>Stay Tuned.</h2>
+                <form method="post" action="#">
+                    <input type="email" name="email" placeholder="Your Email">
+                    <button type="button" name="send-button"><span class="fa fa-arrow-circle-o-right"></span></button>
+                </form>
+            </section>
+    </aside>
+
+    <main class="content">
+            <article>
+                <img src="images/article-img-0.jpg" alt="Duis aute irure dolor in henderit in voluptate.">
+                <h2>Duis aute irure dolor in henderit in voluptate.</h2>
+                <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquipop ex ea commo doconsequat, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur...</p>
+                <a href="#">Continue Reading</a>
+            </article>
+
+            <article>
+                <img src="images/article-img-01.jpg" alt="Duis aute irure dolor in henderit in voluptate.">
+                <h2>Duis aute irure dolor in henderit in voluptate.</h2>
+                <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquipop ex ea commo doconsequat, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur...</p>
+                <a href="#">Continue Reading</a>
+            </article>
+    </main>
+</div>
+
+    <section class="navigation">
+        <div class="container">
+            <ul>
+                <li><a href="#"><span class="fa fa-long-arrow-left"></span>Previous</a></li>
+                <li><a href="#">Next<span class="fa fa-long-arrow-right"></span></a></li>
+            </ul>
+        </div>
+    </section>
+
+<footer>
+    <h2><a href="#" class="logo">Blogin<span>.</span></a></h2>
+    <p>© 2014 Blogin.com  -  All Rights Reserved  -  Find more free Templates at <span>Pixelhint.com<span></span></p>
+    <ul class="social">
+        <li><a href="#"><span class="fa fa-facebook"></span></a></li>
+        <li><a href="#"><span class="fa fa-twitter"></span></a></li>
+        <li><a href="#"><span class="fa fa-behance"></span></a></li>
+        <li><a href="#"><span class="fa fa-dribbble"></span></a></li>
+    </ul>
+</footer>
+
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+<script src="js/script-menu.js"></script>
+</body>
+</html>
